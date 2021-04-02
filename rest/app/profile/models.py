57e060c24b0e54fe6187da33bb25a6e7bfb39a15
unknown <47188867+implicitdefcncdragneel@users.noreply.github.com>
2021-04-02 14:00:01 +0530

    Update models.py

--- rest/app/profile/models.py
+++ rest/app/profile/models.py
@@ -5,6 +5,16 @@ from rest.app.user.models import User
 
 class UserProfile(models.Model):
 
+    USER_TYPES = (
+        ('teacher', 'teacher'),
+        ('student', 'student'),
+        ('employee', 'employee'),
+        ('employer', 'employer'),
+        ('institute', 'institute'),
+    )
+
+     user_type = models.CharField(max_length=1, choices=USER_TYPES)
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     first_name = models.CharField(max_length=50, unique=False)
